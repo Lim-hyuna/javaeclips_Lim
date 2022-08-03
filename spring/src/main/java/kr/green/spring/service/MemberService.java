a0d67320ad71f9093c4052321e5e739ece5874ff
Lim-hyuna <104331562+Lim-hyuna@users.noreply.github.com>
2022-08-03 16:54:36 +0900

    spring: DB연동

--- spring/src/main/java/kr/green/spring/service/MemberService.java
+++ spring/src/main/java/kr/green/spring/service/MemberService.java
@@ -1,8 +1,16 @@
 package kr.green.spring.service;
 
+import kr.green.spring.vo.MemberVo;
+
 public interface MemberService {
 
 	String getEmail(String id);
+
+	MemberVo getMember(String me_id);
+
+	MemberVo getMember(MemberVo member);
+
+	
 
 
 
