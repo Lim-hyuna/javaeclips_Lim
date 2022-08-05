03499ec8baae48c421c90e80944141647e572c39
Lim-hyuna <104331562+Lim-hyuna@users.noreply.github.com>
2022-08-05 12:30:17 +0900

    spring: 게시글 리스트 구현 완료

--- spring/src/main/java/kr/green/spring/service/BoardService.java
+++ spring/src/main/java/kr/green/spring/service/BoardService.java
@@ -1,10 +1,14 @@
 package kr.green.spring.service;
 
+import java.util.ArrayList;
+
 import kr.green.spring.vo.BoardVO;
 import kr.green.spring.vo.MemberVo;
 
 public interface BoardService {
 
 	void insertBoard(BoardVO board, MemberVo user);
+
+	ArrayList<BoardVO> getBoardList();
 
 }
