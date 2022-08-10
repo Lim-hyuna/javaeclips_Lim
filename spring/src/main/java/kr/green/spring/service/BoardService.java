7bfa3a300d27f9978bd1916d0f0a9b6b05f8395d
Lim-hyuna <104331562+Lim-hyuna@users.noreply.github.com>
2022-08-10 17:10:16 +0900

    spring: 게시글 리스트 페이지네이션 적용 완료

--- spring/src/main/java/kr/green/spring/service/BoardService.java
+++ spring/src/main/java/kr/green/spring/service/BoardService.java
@@ -2,6 +2,7 @@ package kr.green.spring.service;
 
 import java.util.ArrayList;
 
+import kr.green.spring.pagination.Criteria;
 import kr.green.spring.vo.BoardVO;
 import kr.green.spring.vo.MemberVo;
 
@@ -9,7 +10,7 @@ public interface BoardService {
 
 	void insertBoard(BoardVO board, MemberVo user);
 
-	ArrayList<BoardVO> getBoardList();
+	ArrayList<BoardVO> getBoardList(Criteria cri);
 
 	void updateViews(Integer bd_num);
 
@@ -18,6 +19,8 @@ public interface BoardService {
 	void updateBoard(BoardVO board, MemberVo user);
 
 	void deleteBoard(Integer bd_num, MemberVo user);
+
+	int getTotalCount();
 
 	
 
